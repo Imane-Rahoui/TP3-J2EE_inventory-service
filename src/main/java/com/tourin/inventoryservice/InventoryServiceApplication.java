1b32baece9e36e719f7525b1cdf24544f6d92a72
Imane-Rahoui <Imaanerahoui@gmail.com>
2022-11-05 20:51:16 +0100

    commit -- exposeId for product

--- src/main/java/com/tourin/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/tourin/inventoryservice/InventoryServiceApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -24,7 +25,8 @@ public class InventoryServiceApplication {
 	}
 
 	@Bean
-	CommandLineRunner start(ProductRepository productRepository){
+	CommandLineRunner start(ProductRepository productRepository, RepositoryRestConfiguration restConfiguration){
+		restConfiguration.exposeIdsFor(Product.class);
 		return args -> {
 			productRepository.save(new Product(null,"Ordinateur",88,12));
 			productRepository.save(new Product(null,"Imprimante",88,15));
